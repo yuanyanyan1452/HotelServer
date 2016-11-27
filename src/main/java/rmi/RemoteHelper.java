b389b5121730392e2b7cb9a207cafc06a517c8e2
xjwhhh <920054996@qq.com>
2016-11-27 20:28:45 +0800

    V1.2
    rmi

--- src/main/java/rmi/RemoteHelper.java
+++ src/main/java/rmi/RemoteHelper.java
@@ -1,5 +1,32 @@
 package rmi;
 
-public class RemoteHelper {
+import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
+import java.rmi.Naming;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 
+public class RemoteHelper {
+	public RemoteHelper(){
+		initServer();
+	}
+	
+	public void initServer(){
+		DataRemoteObject dataRemoteObject;
+		try {
+			dataRemoteObject = new DataRemoteObject();
+			LocateRegistry.createRegistry(8888);
+			Naming.bind("rmi://localhost:8888/DataRemoteObject",
+					(Remote) dataRemoteObject);
+			System.out.println("ready");
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (AlreadyBoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
