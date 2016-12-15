2bc348006e85fbb3f5bdeecd81af005229cfb1b5
xjwhhh <920054996@qq.com>
2016-12-15 16:17:39 +0800

    V3.1.1
    hotelbl的test未完成版

--- src/test/java/test/HotelTest.java
+++ src/test/java/test/HotelTest.java
@@ -1,85 +1,196 @@
-//package test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//import java.util.ArrayList;
-//
-//import org.junit.Test;
-//
-//import mock.MockClient1;
-//import mock.MockHotel1;
-//import mock.MockHotelWorker1;
-//import mock.MockRoom1;
-//import objects.Hotel;
-//import objects.ResultMessage;
-//import service.blservice.Impl.HotelBLServiceImpl;
-//import vo.HotelVO;
-//
-//public class HotelTest {
-//	HotelBLServiceImpl hotelblImpl = new HotelBLServiceImpl();
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import objects.Hotel;
+import objects.HotelWorker;
+import objects.ResultMessage;
+import service.blservice.Impl.HotelBLServiceImpl;
+import vo.AccommodationVO;
+import vo.EvaluationVO;
+import vo.HotelVO;
+import vo.HotelWorkerVO;
+import vo.RoomVO;
+
+public class HotelTest {
+	HotelBLServiceImpl hotelblImpl = new HotelBLServiceImpl();
+	
+	@Test
+	public void testhotelworker_login() throws RemoteException{
+		ResultMessage result=hotelblImpl.hotelworker_login("Matin", "yyj");
+		
+		assertEquals(ResultMessage.Success, result);;
+	}
+	
+	@Test
+	public void testhotelqorker_getvo() throws RemoteException{
+		HotelWorkerVO hotelworkervo=hotelblImpl.hotelworker_getvo("Matin");
+		HotelWorkerVO testhotelworkervo=new HotelWorkerVO();
+		
+		assertEquals(testhotelworkervo,hotelworkervo);
+	}
+	
+	@Test
+	public void testhotelworker_changepassword() throws RemoteException{
+		ResultMessage result=hotelblImpl.hotelworker_change_password("Matin","yyj", "newpassword");
+		
+		assertEquals(ResultMessage.Success, result);
+	}
+	
+	@Test
+	public void testhotel_checkInfo(){
+		HotelVO hotelVO = hotelblImpl.hotel_checkInfo(1);
+		HotelVO testHotel = new HotelVO();
+		
+		assertEquals(testHotel,hotelVO);
+	}
+	
+	@Test
+	public void testhotel_updateInfo(){
+		HotelVO testHotel = new HotelVO();
+		
+		assertEquals(ResultMessage.Success,hotelblImpl.hotel_updateInfo(testHotel));
+	}
+	
+	@Test
+	public void testhotel_importRoom(){
+		RoomVO roomvo=new RoomVO();
+		
+		assertEquals(ResultMessage.Success,hotelblImpl.hotel_importRoom(roomvo));
+	}
+	
+	@Test
+	public void testhotel_updateAccomodation() throws RemoteException{
+		AccommodationVO accomodationvo=new AccommodationVO(null, null, null, null);
+		int orderid =1;
+		
+		assertEquals(ResultMessage.Success,hotelblImpl.hotel_updateAccomodation(accomodationvo,orderid));
+	}
+	
+	@Test
+	public void testsearchHotelByLocation(){
+		ArrayList<HotelVO> testhotelvo_list=new ArrayList<HotelVO>();
+		
+		ArrayList<HotelVO> hotelvo_list=hotelblImpl.searchHotelBylocation("南京","新街口");
+		
+		assertEquals(testhotelvo_list,hotelvo_list);
+	}
+	
 //	@Test
-//	public void testhotel_checkInfo(){
-//		HotelVO hotelVO = hotelblImpl.hotel_checkInfo(1);
-//		HotelVO testHotel = new HotelVO(1,"南京","新街�?", "如家", "如家酒店", "酒店�?有服�?", 1,"可用", 1,1);
+//	public void testsearchHotelByname(){
+//		ArrayList<HotelVO> testhotelvo_list=new ArrayList<HotelVO>();
+//		HotelVO testhotelvo=new HotelVO();
+//		testhotelvo_list.add(testhotelvo);
+//		ArrayList<HotelVO> hotelvo_list=hotelblImpl.searchHotelByname("");
 //		
-//		assertEquals(testHotel,hotelVO);
 //	}
-//	
+//
 //	@Test
-//	public void testhotel_updateInfo(){
-//		HotelVO testHotel = new HotelVO(1,"南京","仙林中心", "如家", "如家酒店", "酒店�?有服�?", 1,"可用", 1,1);
-//		assertEquals(ResultMessage.Success,hotelblImpl.hotel_updateInfo(testHotel));
+//	public void testsearchHotelByroom(){
+//		ArrayList<HotelVO> testhotelvo_list=new ArrayList<HotelVO>();
+//		HotelVO testhotelvo=new HotelVO();
+//		testhotelvo_list.add(testhotelvo);
+//		ArrayList<HotelVO> hotelvo_list=hotelblImpl.searchHotelBylocation("南京","新街口");
+//		
 //	}
-//	
+//
 //	@Test
-//	public void testhotel_importRoom(){
-//		MockRoom1 importRoom = new MockRoom1("双人�?","220",5,"2016-10-11","2016-11-1");
-//		assertEquals(ResultMessage.Success,hotelblImpl.hotel_importRoom(importRoom));
+//	public void testsearchHotelByprice(){
+//		ArrayList<HotelVO> testhotelvo_list=new ArrayList<HotelVO>();
+//		HotelVO testhotelvo=new HotelVO();
+//		testhotelvo_list.add(testhotelvo);
+//		ArrayList<HotelVO> hotelvo_list=hotelblImpl.searchHotelBylocation("南京","新街口");
+//		
 //	}
-//	
+//
 //	@Test
-//	public void testhotel_updateAccomodation(){
-////		MockAccommo1 info = new MockAccommo1("201","2016-10-11","2016-11-03","2016_11-04");
-////		assertEquals(ResultMessage.Success,hotelblImpl.hotel_updateAccomodation(info));
+//	public void testsearchHotelBytime(){
+//		ArrayList<HotelVO> testhotelvo_list=new ArrayList<HotelVO>();
+//		HotelVO testhotelvo=new HotelVO();
+//		testhotelvo_list.add(testhotelvo);
+//		ArrayList<HotelVO> hotelvo_list=hotelblImpl.searchHotelBylocation("南京","新街口");
+//		
 //	}
-//	
+//
 //	@Test
-//	public void testsearchHotel(){
-//		Hotel hotel = new Hotel();
-//		MockHotel1 hotel1 = new MockHotel1(1);
-//		assertEquals(hotel,hotelblImpl.searchHotel(hotel1.getHotelid()));
+//	public void testsearchHotelBystar(){
+//		ArrayList<HotelVO> testhotelvo_list=new ArrayList<HotelVO>();
+//		HotelVO testhotelvo=new HotelVO();
+//		testhotelvo_list.add(testhotelvo);
+//		ArrayList<HotelVO> hotelvo_list=hotelblImpl.searchHotelBylocation("南京","新街口");
+//		
 //	}
-//	
+//
 //	@Test
-//	public void testpreviousHotel(){
-//		ArrayList<Hotel> HotelList_Client=new ArrayList<Hotel>();
-//		Hotel hotel=new Hotel();
-//		HotelList_Client.add(hotel);
-//		MockClient1 client = new MockClient1(2);
-//		assertEquals(HotelList_Client,hotelblImpl.previousHotel(client.getClientid()));
+//	public void testsearchHotelByscore(){
+//		ArrayList<HotelVO> testhotelvo_list=new ArrayList<HotelVO>();
+//		HotelVO testhotelvo=new HotelVO();
+//		testhotelvo_list.add(testhotelvo);
+//		ArrayList<HotelVO> hotelvo_list=hotelblImpl.searchHotelBylocation("南京","新街口");
+//		
 //	}
-//	
-//	@Test
-//	public void testaddHotel(){
-//		Hotel hotel = new Hotel();
-//		assertEquals(ResultMessage.Success,hotelblImpl.addHotel(hotel));
-//	}
-//	
-//	@Test
-//	public void testaddHotelWorker(){
-//		MockHotelWorker1 worker = new MockHotelWorker1(1,"张三","10000","如家");
-//		assertEquals(ResultMessage.Success,hotelblImpl.addHotelWorker(worker));
-//	}
-//	
-//	@Test
-//	public void testsearchHotelWorker(){
-//		MockHotelWorker1 worker = new MockHotelWorker1(1,"张三","10000","如家");
-//		assertEquals(worker,hotelblImpl.searchHotelWorker(1));
-//	}
-//	
-//	@Test
-//	public void testupdateHotelWokerInfo(){
-//		MockHotelWorker1 worker = new MockHotelWorker1(1,"张三","10110","如家");
-//		assertEquals(ResultMessage.Success,hotelblImpl.updateHotelWokerInfo(worker.getHotelid(), worker));
-//	}
-//}
+	
+	@Test
+	public void testpreviousHotel(){
+		ArrayList<Hotel> testHotelList_Client=new ArrayList<Hotel>();
+		Hotel hotel=new Hotel();
+		testHotelList_Client.add(hotel);
+		ArrayList<Hotel> HotelList=hotelblImpl.previousHotel(1);
+		
+		assertEquals(testHotelList_Client,HotelList);
+	}
+	
+	@Test
+	public void testaddHotel(){
+		Hotel hotel = new Hotel();
+		
+		assertEquals(ResultMessage.Success,hotelblImpl.addHotel(hotel));
+	}
+	
+	@Test
+	public void testaddHotelWorker(){
+		HotelWorker hotelworker=new HotelWorker();
+		ResultMessage result=hotelblImpl.addHotelWorker(hotelworker);
+		
+		assertEquals(ResultMessage.Success,result);
+	}
+	
+	@Test
+	public void testsearchHotelWorker(){
+		HotelWorker testhotekworker=new HotelWorker();
+		HotelWorker hotelworker=hotelblImpl.searchHotelWorker(1);
+		
+		assertEquals(testhotekworker,hotelworker);
+	}
+	
+	@Test
+	public void testupdateHotelWokerInfo(){
+		HotelWorker hotelworker=new HotelWorker();
+		ResultMessage result=hotelblImpl.updateHotelWokerInfo(hotelworker);
+		
+		assertEquals(ResultMessage.Success,result);
+	}
+	
+	@Test
+	public void testevaluatehotel() throws RemoteException{
+		EvaluationVO e=new EvaluationVO(0, null);
+		int clientid=1;
+		int hotelid=1;
+		ResultMessage result=hotelblImpl.evalutehotel(e,clientid,hotelid);
+		
+		assertEquals(ResultMessage.Success, result);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
